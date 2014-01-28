98136988956adac2ce3d4a2d0b4f2290c0a1f681
Connor Peet <connor@connorpeet.com>
2014-01-28 09:50:57 -0600

    Resource usage graphs, minor tweaks

--- panel/protected/views/server/resources.php
+++ panel/protected/views/server/resources.php
@@ -1,28 +1,33 @@
 <?php if ($cpu !== null || $memory !== null): ?>
-<br/>
-<br/>
-<table style="width: 100%" class="stdtable">
-<tr class="titlerow"> 
-    <td><?php echo Yii::t('mc', 'Resource usage') ?></td>
-</tr>
-<tr class="linerow">
-    <td></td>
-</tr>
-<tr><td></td></tr>
-</table>
-<div style="float: left; width: 50%"><?php echo Yii::t('mc', 'CPU') ?></div>
-<div style="float: left"><?php echo Yii::t('mc', 'Memory') ?></div>
-<div style="clear: both"></div>
-<div style="float: left; width: 50%">
-    <div id="resource_cpu" style="width: 82%; border: 1px solid #555; position: relative">
-        <div style="width: <?php echo $cpu ?>%; background-color: #C6D880; height: 100%">&nbsp;</div>
-        <div style="width: 100%; position: absolute; top: 0; left: 0; height: 100%; text-align: center"><?php echo $cpu ?>%</div>
-    </div>
+
+<?php
+function intervals($value, $warning, $danger) {
+	if ($value >= $danger) {
+		return 'progress-bar-danger';
+	} elseif ($value >= $warning) {
+		return 'progress-bar-warning';
+	}
+
+	return '';
+}
+?>
+<h3><?php echo Yii::t('mc', 'Resource usage') ?></h3>
+<div class="row">
+	<div class="col-md-6">
+		<h4><?php echo Yii::t('mc', 'CPU') ?></h4>
+		<div class="progress progress-striped active">
+			<div class="progress-bar <?php echo intervals($cpu, 85, 95) ?>" role="progressbar" aria-valuenow="<?php echo $cpu ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $cpu ?>%">
+				<?php echo $cpu ?>% Usage
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		<h4><?php echo Yii::t('mc', 'Memory') ?></h4>
+		<div class="progress progress-striped active">
+			<div class="progress-bar <?php echo intervals($memory, 85, 95) ?>" role="progressbar" aria-valuenow="<?php echo $memory ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $memory ?>%">
+				<?php echo $memory ?>% Usage
+			</div>
+		</div>
+	</div>
 </div>
-<div id="resource_cpu" style="float: left; width: 41%; border: 1px solid #555; position: relative">
-    <div style="width: <?php echo $memory ?>%; background-color: #C6D880; height: 100%">&nbsp;</div>
-    <div style="width: 100%; position: absolute; top: 0; left: 0; height: 100%; text-align: center"><?php echo $memory ?>%</div>
-</div>
-<div style="clear: both"></div>
-<br/>
 <?php endif ?>
