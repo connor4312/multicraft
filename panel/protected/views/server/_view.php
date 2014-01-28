c5b33b140b8ea6c80783e79e6297a29d82ef04ef
Connor Peet <connor@connorpeet.com>
2014-01-28 10:07:51 -0600

    Server list

--- panel/protected/views/server/_view.php
+++ panel/protected/views/server/_view.php
@@ -7,7 +7,7 @@
  *
  **/
 ?>
-<div class="view">
+<div class="row server-list" onclick="window.location='<?php echo CHTML::normalizeUrl(array('view', 'id'=>$data->id)) ?>'">
     <?php
     $pl = 0;
     $img = $data->suspended ? '<i class="fa fa-times"></i>' : '<i class="fa fa-refresh fa-spin"></i>';
@@ -17,47 +17,38 @@
         $img = $pl >= 0 ? '<i class="fa fa-check"></i>' : '<i class="fa fa-times"></i>';
     }
     ?>
-    <table style="height: 100%; table-layout: fixed">
-    <colgroup>
-        <col style="width: 35px"/>
-        <col/>
-        <col/>
-    </colgroup>
-    <tr>
-        <td style="padding-left: 10px" id="sv_icon_<?php echo $data->id ?>">
-            <?php echo $img ?>
-        </td>
-        <td style="width: 50%; max-width: 330px; overflow: hidden; vertical-align: middle">
-            <?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id'=>$data->id), array('style'=>'display: block; max-height: 28px')); ?></a>
-        </td>
-        <td>
-            <span id="sv_status_<?php echo $data->id ?>">
-            <?php
-            if ($data->suspended)
-                echo Yii::t('mc', 'Suspended').'</span>';
-            else
+    <div class="col-lg-1">
+        <?php echo $img ?>
+    </div>
+    <div class="col-lg-6">
+        <?php echo CHtml::link(CHtml::encode($data->name), '#'); ?></a>
+    </div>
+    <div class="col-md-5">
+        <span id="sv_status_<?php echo $data->id ?>">
+        <?php
+        if ($data->suspended)
+            echo Yii::t('mc', 'Suspended').'</span>';
+        else
+        {
+            if (!Yii::app()->params['ajax_serverlist'])
             {
-                if (!Yii::app()->params['ajax_serverlist'])
-                {
-                    if ($pl >= 0)
-                        echo CHtml::encode($pl).' /  '.CHtml::encode($data->players).' '.Yii::t('mc', 'Players');
-                    else
-                        echo Yii::t('mc', 'Offline').($pl == -2 ? ' ('.Yii::t('mc', 'error').')' : '');
-                }
+                if ($pl >= 0)
+                    echo CHtml::encode($pl).' /  '.CHtml::encode($data->players).' '.Yii::t('mc', 'Players');
                 else
-                    echo Yii::t('mc', 'Pending');
-                echo '</span>';
-                echo '<span id="sv_maxplr_'.$data->id.'" style="display: none"> /  '.CHtml::encode($data->players).' '.Yii::t('mc', 'Players').'</span>';
-                if ($pl >= 0 && Yii::app()->user->can($data->id, 'chat'))
-                {
-                    echo '<span id="sv_chatlink_'.$data->id.'"'.(Yii::app()->params['ajax_serverlist'] ? ' style="display: none"' : '')
-                        .'> ('.CHtml::link(Yii::t('mc', 'Chat'), array('chat', 'id'=>$data->id)).')</span>';
-                }
+                    echo Yii::t('mc', 'Offline').($pl == -2 ? ' ('.Yii::t('mc', 'error').')' : '');
             }
-            ?>
-        </td>
-    </tr>
-    </table>
+            else
+                echo Yii::t('mc', 'Pending');
+            echo '</span>';
+            echo '<span id="sv_maxplr_'.$data->id.'" style="display: none"> /  '.CHtml::encode($data->players).' '.Yii::t('mc', 'Players').'</span>';
+            if ($pl >= 0 && Yii::app()->user->can($data->id, 'chat'))
+            {
+                echo '<span id="sv_chatlink_'.$data->id.'"'.(Yii::app()->params['ajax_serverlist'] ? ' style="display: none"' : '')
+                    .'> ('.CHtml::link(Yii::t('mc', 'Chat'), array('chat', 'id'=>$data->id)).')</span>';
+            }
+        }
+        ?>
+    </div>
     <?php
         if (!!Yii::app()->params['ajax_serverlist'] && !$data->suspended)
             echo CHtml::script('get_status('.$data->id.');')
