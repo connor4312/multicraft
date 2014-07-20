81cc4148b0711de122a4a42cd4dcbff9ce2ec641
Connor Peet <connor@connorpeet.com>
2014-07-20 09:55:33 -0500

    Fix shadow artifact on some browsers

--- panel/protected/views/layouts/main.php
+++ panel/protected/views/layouts/main.php
@@ -1,10 +1,10 @@
 <?php $this->renderPartial('//layouts/components/head'); ?>
-    <div class="container">
-        
-        <?php $this->renderPartial('//layouts/components/banner'); ?>
-        <?php $this->renderPartial('//layouts/components/navigation'); ?>
+<div class="container">
 
-        <div class="row" id="content"><?php echo $content; ?></div>
+    <?php $this->renderPartial('//layouts/components/banner'); ?>
+    <?php $this->renderPartial('//layouts/components/navigation'); ?>
 
-    </div>
+    <div class="row" id="content"><?php echo $content; ?></div>
+    <div class="shadow"></div>
+</div>
 <?php $this->renderPartial('//layouts/components/foot'); ?>
